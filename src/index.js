b51f8123316aaf7227f5a8c1c76c89de1b24a673
visualdenniss <visualdennis@gmail.com>
2023-03-29 15:16:42 +0200

    tried to import font face globally

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import './fonts/PPNeueMontreal-Medium.otf'
+import './fonts/PPNeueMontreal-Bold.otf'
+import './fonts/PPNeueMontreal-Book.otf'
+import './fonts/PPNeueMontreal-Thin.otf'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
